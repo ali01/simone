0efa31d22d6e199b43094c1e4608c573f70f86fa
Ali Yahya <git@ali01.com>
2010-03-13 01:44:19 -0800

    hdg for math.h

--- math.h
+++ math.h
@@ -1,6 +1,8 @@
 /* Copyright (c) 2008-2010. Ali H. Yahya, All rights reserved. */
 
-#pragma once
+#ifndef MATH_H_NJRC8OE6
+#define MATH_H_NJRC8OE6
+
 #include <cmath>
 
 namespace Simone {
@@ -8,9 +10,12 @@ namespace math {
 
 const double kEpsilon = 0.00001;
 
-bool equal(double lhs, double rhs) {
+inline bool equal(double lhs, double rhs) {
    return fabs(lhs - rhs) < kEpsilon;
 }
 
 } /* end of namespace math */
 } /* end of namespace Simone */
+
+
+#endif
