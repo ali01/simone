a082b3eda07de01727bd2e22d8d0b111a56cc985
Ali Yahya <git@ali01.com>
2010-03-14 20:45:58 -0700

    exception.h formatting

--- exception.h
+++ exception.h
@@ -1,5 +1,5 @@
 /* Copyright (c) 2008-2010. Ali H. Yahya, All rights reserved. */
-/* Adapted from David R. Cheriton's Advanced Object Oriented Programming from a 
+/* Adapted from David R. Cheriton's Advanced Object Oriented Programming from a
    Modeling & Simulation's Perspective ~ Chapter 7: Exceptions & Exception Handling */
 
 #pragma once
@@ -29,125 +29,125 @@ private:
 
 class IncompleteInitializationException : public Exception {
 public:
-   IncompleteInitializationException(string filename, uint32_t line, string msg) :
-                                                   Exception(filename, line, msg) {}
+   IncompleteInitializationException(string filename, uint32_t ln, string msg):
+      Exception(filename, ln, msg) {}
 };
 
 class NoImplementationException : public Exception {
 public:
    NoImplementationException(string filename, uint32_t line, string msg) :
-                                                      Exception(filename, line, msg) {} 
+      Exception(filename, line, msg) {}
 };
 
 class AttributeNotSupportedException : public NoImplementationException {
 public:
    AttributeNotSupportedException(string filename, uint32_t line, string msg) :
-                                       NoImplementationException(filename, line, msg) {} 
+      NoImplementationException(filename, line, msg) {}
 };
 
 class UnknownTypeException : public Exception {
 public:
    UnknownTypeException(string filename, uint32_t line, string msg) :
-                                                      Exception(filename, line, msg) {}
+      Exception(filename, line, msg) {}
 };
 
 class UnknownAttrException : public Exception {
 public:
    UnknownAttrException(string filename, uint32_t line, string msg) :
-                                                      Exception(filename, line, msg) {}
+      Exception(filename, line, msg) {}
 };
 
 class UnknownDelimiterException : public Exception {
 public:
    UnknownDelimiterException(string filename, uint32_t line, string msg) :
-                                                      Exception(filename, line, msg) {}
+      Exception(filename, line, msg) {}
 };
 
 class UnknownArgException : public Exception {
 public:
    UnknownArgException(string filename, uint32_t line, string msg) :
-                                                      Exception(filename, line, msg) {}
+      Exception(filename, line, msg) {}
 };
 
 
 class RangeException : public Exception {
 public:
    RangeException(string filename, uint32_t line, string msg) :
-                                                      Exception(filename, line, msg) {} 
+      Exception(filename, line, msg) {}
 };
 
 class MemoryException : public Exception {
  public:
    MemoryException(string filename, uint32_t line, string msg) :
-                                                      Exception(filename, line, msg) {} 
+      Exception(filename, line, msg) {}
 };
 
 class StorageException : public Exception {
  public:
    StorageException(string filename, uint32_t line, string msg) :
-                                                      Exception(filename, line, msg) {}
+      Exception(filename, line, msg) {}
 };
 
 class NameInUseException : public Exception {
 public:
    NameInUseException(string filename, uint32_t line, string msg) :
-                                                      Exception(filename, line, msg) {} 
-   
+      Exception(filename, line, msg) {}
+
 };
 
 class IllegalNameException: public Exception {
  public:
    IllegalNameException(string filename, uint32_t line, string msg) :
-                                                      Exception(filename, line, msg) {}
-   
+      Exception(filename, line, msg) {}
+
 };
 
 class EntityNotFoundException : public Exception {
 public:
    EntityNotFoundException(string filename, uint32_t line, string msg) :
-                                                      Exception(filename, line, msg) {} 
-   
+      Exception(filename, line, msg) {}
+
 };
 
 
 class MemoryLimitExceededException : public MemoryException {
 public:
    MemoryLimitExceededException(string filename, uint32_t line, string msg) :
-                                                MemoryException(filename, line, msg) {}
-   
+      MemoryException(filename, line, msg) {}
+
 };
 
 class ReadOnlyException : public Exception {
 public:
    ReadOnlyException(string filename, uint32_t line, string msg) :
-                                                      Exception(filename, line, msg) {}
-   
+      Exception(filename, line, msg) {}
+
 };
 
 class InvalidFormattingException: public Exception {
 public:
    InvalidFormattingException(string filename, uint32_t line, string msg) :
-                                                      Exception(filename, line, msg) {}
-   
+      Exception(filename, line, msg) {}
+
 };
 
 class InvalidOperationException: public Exception {
 public:
    InvalidOperationException(string filename, uint32_t line, string msg) :
-                                                      Exception(filename, line, msg) {}
-   
+      Exception(filename, line, msg) {}
+
 };
 
 class InvalidValueException: public Exception {
 public:
    InvalidValueException(string filename, uint32_t line, string msg) :
-                                                      Exception(filename, line, msg) {}
+      Exception(filename, line, msg) {}
 };
 
 class NullPointerException : public InvalidValueException {
 public:
    NullPointerException(string filename, uint32_t line, string msg) :
-                                          InvalidValueException(filename, line, msg) {}
+      InvalidValueException(filename, line, msg) {}
 };
 
 } /* end of namespace Simone */
