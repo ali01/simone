485c2083bd77165d61a23ae030c1b7ce0d4696c2
Ali Yahya <git@ali01.com>
2010-04-25 14:25:16 -0700

    fine-tuned ordinal

--- ordinal.h
+++ ordinal.h
@@ -1,61 +1,140 @@
 /* Copyright (c) 2008-2010. Ali H. Yahya, All rights reserved. */
+/* Adapted from David R. Cheriton's Advanced Object Oriented Programming from a
+  Modeling & Simulation's Perspective ~ Chapter 16: Value-Oriented Programming &
+  Value Types, Numerical class implementation. */
 
-#ifndef ORDINAL_H_ENSZR7PR
-#define ORDINAL_H_ENSZR7PR
+#ifndef ORDINAL_H_G09I1KEU
+#define ORDINAL_H_G09I1KEU
 
+#include <sstream>
 #include <ostream>
-
-#include "thread/concurrent_collection_element.h"
+#include <limits>
+#include <string>
+using std::stringstream;
+using std::ostream;
+using std::numeric_limits;
+using std::string;
 
 namespace Simone {
-using std::ostream;
 
-template <typename UnitType,typename RepType>
-class Ordinal : public thread::ConcurrentCollectionElement {
+template<typename UnitType, typename RepType>
+class Ordinal {
 public:
-  Ordinal(RepType _v) : value_(_v) {}
-  Ordinal(const Ordinal<UnitType,RepType>& _o) : value_(_o.value()) {}
+  /* constructors */
+  Ordinal() { valueIs(0); }
+  Ordinal(RepType v) { valueIs(v); }
+  Ordinal(const Ordinal<UnitType,RepType>& v) { valueIs(v.value_); }
 
-  virtual
-  Ordinal<UnitType,RepType>& operator=(const Ordinal<UnitType,RepType>& _o) {
-    if (this != &_o) { value_ = _o.value(); }
-    return *this;
+  /* default virtual destructor */
+  virtual ~Ordinal() {}
+
+  /* member functions */
+  
+  RepType value() const { return value_; }
+  
+  virtual const string str() const {
+    stringstream ss;
+    ss << *this;
+    return ss.str();
+  }
+  
+  /* static functions */
+  
+  static RepType maxValue() {
+    #ifdef WIN32
+    #undef max
+      return numeric_limits<RepType>::max();
+    #else
+      return numeric_limits<RepType>::max();
+    #endif
   }
 
-  virtual bool operator==(const Ordinal<UnitType,RepType>& _o) const {
-    return value() == _o.value();
+  static RepType minValue() {
+    #ifdef WIN32
+    #undef min
+      return numeric_limits<RepType>::min();
+    #else
+      return numeric_limits<RepType>::min();
+    #endif
   }
+  
+  /* assignment */
+  
+  const Ordinal<UnitType,RepType>&
+    operator=(const Ordinal<UnitType,RepType>& v);
 
-  virtual bool operator!=(const Ordinal<UnitType,RepType>& _o) const {
-    return value() != _o.value();
+  /* relational operators */
+  
+  bool operator==(const Ordinal<UnitType,RepType>& v) const;
+  bool operator!=(const Ordinal<UnitType,RepType>& v) const;
+  bool operator>=(const Ordinal<UnitType,RepType>& v) const;
+  bool operator<=(const Ordinal<UnitType,RepType>& v) const;
+  bool operator< (const Ordinal<UnitType,RepType>& v) const;
+  bool operator> (const Ordinal<UnitType,RepType>& v) const;
+
+  /* stream operator */
+
+  friend
+  ostream& operator<<(ostream& out, const Ordinal<UnitType,RepType>& _n) {
+    return out << _n.value_;
   }
-
-  virtual bool operator< (const Ordinal<UnitType,RepType>& _o) const {
-    return value() < _o.value();
-  }
-
-  virtual bool operator<=(const Ordinal<UnitType,RepType>& _o) const {
-    return value() <= _o.value();
-  }
-
-  virtual bool operator> (const Ordinal<UnitType,RepType>& _o) const {
-    return value() > _o.value();
-  }
-
-  virtual bool operator>=(const Ordinal<UnitType,RepType>& _o) const {
-    return value() >= _o.value();
-  }
-
-  friend ostream&
-  operator<<(ostream& out, const Ordinal<UnitType,RepType>& _o) {
-    return out << _o.value();
-  }
-
-  virtual RepType value() const { return value_; }
-
 protected:
+  /* member functions */
+  virtual void valueIs(RepType v) { value_ = v; }
+  virtual bool equal(RepType v) const {
+    return value_ == v;
+  }
+
+  /* data members */
   RepType value_;
 };
+
+/* assignment */
+template<typename UnitType, typename RepType>
+inline const Ordinal<UnitType,RepType>&
+Ordinal<UnitType,RepType>::operator=(const Ordinal<UnitType,RepType>& v) {
+  if (this != &v) { valueIs(v.value_); }
+  return v;
+}
+
+
+/* relational operators */
+
+template<typename UnitType, typename RepType>
+inline bool
+Ordinal<UnitType,RepType>::operator==(const Ordinal<UnitType,RepType>& v) const {
+  return equal(v.value());
+}
+
+template<typename UnitType, typename RepType>
+inline bool
+Ordinal<UnitType,RepType>::operator!=(const Ordinal<UnitType,RepType>& v) const {
+  return !equal(v.value());
+}
+
+template<typename UnitType, typename RepType>
+inline bool
+Ordinal<UnitType,RepType>::operator<=(const Ordinal<UnitType,RepType>& v) const {
+  return value_ <= v.value_;
+}
+
+template<typename UnitType, typename RepType>
+inline bool
+Ordinal<UnitType,RepType>::operator>=(const Ordinal<UnitType,RepType>& v) const {
+  return value_ >= v.value_;
+}
+
+template<typename UnitType, typename RepType>
+inline bool
+Ordinal<UnitType,RepType>::operator<(const Ordinal<UnitType,RepType>& v) const {
+  return value_ < v.value_;
+}
+
+template<typename UnitType, typename RepType>
+inline bool
+Ordinal<UnitType,RepType>::operator>(const Ordinal<UnitType,RepType>& v) const {
+  return value_ > v.value_;
+}
 
 } /* end of namespace Simone */
 
