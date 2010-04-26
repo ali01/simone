4004a3a37becc5d354d21827efbe0e4c2a208933
Ali Yahya <git@ali01.com>
2010-04-26 16:26:29 -0700

    fixed race condition in concurrent_map; created map (thread unsafe) class

---
+++ map.h
@@ -0,0 +1,106 @@
+/* Copyright (c) 2008-2010. Ali H. Yahya, All rights reserved. */
+
+#pragma once
+#include <map>
+#include <functional>
+using std::map;
+using std::less;
+
+#include "ptr_interface.h"
+#include "utility.h"
+
+namespace Simone {
+
+template<typename KeyT,typename ValueT,typename Cmp=less<KeyT> >
+class Map : public PtrInterface<Map<KeyT,ValueT,Cmp> >, boost::noncopyable {
+public:
+  typedef Simone::Ptr<const Map<KeyT,ValueT,Cmp> > PtrConst;
+  typedef Simone::Ptr<Map<KeyT,ValueT,Cmp> > Ptr;
+  
+  typedef typename map<KeyT,ValueT,Cmp>::iterator iterator;
+
+  typedef typename map<KeyT,ValueT,Cmp>::reverse_iterator reverse_iterator;
+
+  typedef typename map<KeyT,ValueT,Cmp>::const_iterator const_iterator;
+
+  typedef typename map<KeyT,ValueT,Cmp>::const_reverse_iterator
+                                           const_reverse_iterator;
+  
+  static Ptr MapNew() { return new Map(); }
+  
+  Map() {}
+  
+  virtual ~Map() {}
+  
+  iterator begin() {
+    return map_.begin();
+  }
+  
+  iterator end() {
+    return map_.end();
+  }
+  
+  reverse_iterator rbegin() {
+    return map_.rbegin();
+  }
+  
+  reverse_iterator rend() {
+    return map_.rend();
+  }
+  
+  const_iterator begin() const {
+    return map_.begin();
+  }
+  
+  const_iterator end() const {
+    return map_.end();
+  }
+  
+  const_reverse_iterator rbegin() const {
+    return map_.rbegin();
+  }
+  
+  const_reverse_iterator rend()   const {
+    return map_.rend();
+  }
+  
+  size_t size() const {
+    return map_.size();
+  }
+  
+  bool empty() const {
+    return map_.empty();
+  }
+  
+  iterator element(const KeyT& _key) {
+    return map_.find(_key);
+  }
+  const_iterator element(const KeyT& _key) const {
+    return map_.find(_key);
+  }
+  
+  void elementIs(const KeyT& _key, const ValueT& _v) {
+    _v.collectionIs(this);
+    map_[_key] = _v;
+  }
+  
+  void elementDel(iterator _it) {
+    map_.erase(_it);
+  }
+  void elementDel(const KeyT& _key) {
+    map_.erase(_key);
+  }
+  
+  ValueT& operator[](const KeyT& key) {
+    return map_[key];
+  }
+  
+  const ValueT& operator[](const KeyT& key) const {
+    return map_[key];
+  }
+private:
+  /* data members */
+  map<KeyT,ValueT,Cmp> map_;
+};
+
+} /* end of namespace Simone */
--- thread/concurrent_map.h
+++ thread/concurrent_map.h
@@ -15,119 +15,116 @@ namespace Simone {
 namespace thread {
 
 template <typename KeyType,
-          typename ValueType,
-          typename Compare=less<KeyType> >
+       typename ValueType,
+       typename Cmp=less<KeyType> >
 class ConcurrentMap : public ConcurrentCollection {
 public:
-   // type declarations ==============================================================
-   typedef Simone::Ptr<const ConcurrentMap<KeyType,ValueType,Compare> > PtrConst;
-   typedef Simone::Ptr<ConcurrentMap<KeyType,ValueType,Compare> > Ptr;
-   
-   typedef typename map<KeyType,ValueType,Compare>::iterator
-                                                    iterator;
-   typedef typename map<KeyType,ValueType,Compare>::reverse_iterator 
-                                                    reverse_iterator;
-   typedef typename map<KeyType,ValueType,Compare>::const_iterator
-                                                    const_iterator;
-   typedef typename map<KeyType,ValueType,Compare>::const_reverse_iterator
-                                                    const_reverse_iterator;
-   
-   // factory constructor ============================================================
-   static Ptr ConcurrentMapNew() { return new ConcurrentMap(); }
-   ConcurrentMap() {}
-   
-   virtual ~ConcurrentMap() {
-      ScopedLock lk(this->mutex());
-   }
-   
-   // iterators ======================================================================
-   iterator begin() {
-      ScopedLock lk(this->mutex());
-      return map_.begin();
-   }
-   
-   iterator end() {
-      ScopedLock lk(this->mutex());
-      return map_.end();
-   }
-   
-   reverse_iterator rbegin() {
-      ScopedLock lk(this->mutex());
-      return map_.rbegin();
-   }
-   
-   reverse_iterator rend() {
-      ScopedLock lk(this->mutex());
-      return map_.rend();
-   }
-   
-   const_iterator begin() const {
-      ScopedLock lk(this->mutex());
-      return map_.begin();
-   }
-   
-   const_iterator end() const {
-      ScopedLock lk(this->mutex());
-      return map_.end();
-   }
-   
-   const_reverse_iterator rbegin() const {
-      ScopedLock lk(this->mutex());
-      return map_.rbegin();
-   }
-   
-   const_reverse_iterator rend()   const {
-      ScopedLock lk(this->mutex());
-      return map_.rend();
-   }
-   
-   // accessors  =====================================================================
-   size_t size()  const {
-      ScopedLock lk(this->mutex());
-      return map_.size();
-   }
-   
-   bool empty() const {
-      ScopedLock lk(this->mutex());
-      return map_.empty();
-   }
-   
-   iterator element(const KeyType& _key) {
-      ScopedLock lk(this->mutex());
-      return map_.find(_key);
-   }
-   const_iterator element(const KeyType& _key) const {
-      ScopedLock lk(this->mutex());
-      return map_.find(_key);
-   }
-   
-   // mutators =======================================================================
-   void elementIs(const KeyType& _key, const ValueType& _v) {
-      _v.collectionIs(this);
-      map_[_key] = _v;
-   }
-   
-   void elementDel(iterator _it) {
-      ScopedLock lk(this->mutex());
-      map_.erase(_it);
-   }
-   void elementDel(const KeyType& _key) {
-      map_.erase(_key);
-   }
-   
-   ValueType& operator[](const KeyType& key) {
-      ScopedLock lk(this->mutex());
-      return map_[key];
-   }
-   
-   const ValueType& operator[](const KeyType& key) const {
-      ScopedLock lk(this->mutex());
-      return map_[key];
-   }
+  typedef Simone::Ptr<const ConcurrentMap<KeyType,ValueType,Cmp> > PtrConst;
+  typedef Simone::Ptr<ConcurrentMap<KeyType,ValueType,Cmp> > Ptr;
+  
+  typedef typename map<KeyType,ValueType,Cmp>::iterator iterator;
+
+  typedef typename map<KeyType,ValueType,Cmp>::reverse_iterator
+                                                reverse_iterator;
+
+  typedef typename map<KeyType,ValueType,Cmp>::const_iterator const_iterator;
+
+  typedef typename map<KeyType,ValueType,Cmp>::const_reverse_iterator
+                                                 const_reverse_iterator;
+  
+  static Ptr ConcurrentMapNew() { return new ConcurrentMap(); }
+  ConcurrentMap() {}
+  
+  virtual ~ConcurrentMap() {
+    ScopedLock lk(this->mutex());
+  }
+  
+  iterator begin() {
+    ScopedLock lk(this->mutex());
+    return map_.begin();
+  }
+  
+  iterator end() {
+    ScopedLock lk(this->mutex());
+    return map_.end();
+  }
+  
+  reverse_iterator rbegin() {
+    ScopedLock lk(this->mutex());
+    return map_.rbegin();
+  }
+  
+  reverse_iterator rend() {
+    ScopedLock lk(this->mutex());
+    return map_.rend();
+  }
+  
+  const_iterator begin() const {
+    ScopedLock lk(this->mutex());
+    return map_.begin();
+  }
+  
+  const_iterator end() const {
+    ScopedLock lk(this->mutex());
+    return map_.end();
+  }
+  
+  const_reverse_iterator rbegin() const {
+    ScopedLock lk(this->mutex());
+    return map_.rbegin();
+  }
+  
+  const_reverse_iterator rend()   const {
+    ScopedLock lk(this->mutex());
+    return map_.rend();
+  }
+  
+  size_t size() const {
+    ScopedLock lk(this->mutex());
+    return map_.size();
+  }
+  
+  bool empty() const {
+    ScopedLock lk(this->mutex());
+    return map_.empty();
+  }
+  
+  iterator element(const KeyType& _key) {
+    ScopedLock lk(this->mutex());
+    return map_.find(_key);
+  }
+  const_iterator element(const KeyType& _key) const {
+    ScopedLock lk(this->mutex());
+    return map_.find(_key);
+  }
+  
+  void elementIs(const KeyType& _key, const ValueType& _v) {
+    ScopedLock lk(this->mutex());
+    _v.collectionIs(this);
+    map_[_key] = _v;
+  }
+  
+  void elementDel(iterator _it) {
+    ScopedLock lk(this->mutex());
+    map_.erase(_it);
+  }
+  void elementDel(const KeyType& _key) {
+    ScopedLock lk(this->mutex());
+    map_.erase(_key);
+  }
+  
+  ValueType& operator[](const KeyType& key) {
+    ScopedLock lk(this->mutex());
+    return map_[key];
+  }
+  
+  const ValueType& operator[](const KeyType& key) const {
+    ScopedLock lk(this->mutex());
+    return map_[key];
+  }
 private:
-   // member functions ===============================================================
-   // data members ===================================================================
-   map<KeyType,ValueType,Compare> map_;
+  /* data members */
+  map<KeyType,ValueType,Cmp> map_;
 };
 
 } /* end of namespace thread */
