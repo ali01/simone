253c4d975eb1e5b445cd7a86729a296e252c776e
Ali Yahya <alive@ali01.com>
2010-05-09 16:08:55 -0700

    database create query functionality

--- database/sqlite_db.cpp
+++ database/sqlite_db.cpp
@@ -5,18 +5,30 @@
 namespace Simone {
 
 SQLiteDB::SQLiteDB(const string& _db_name) {
+  /* - OPEN_CREATE creates database if it doesn't already exist;
+     - OPEN_NOMUTEX allows multithreaded access to database as long as any
+                    single sqlite3* abject is always accessed by one thread; */
   int flags = SQLITE_OPEN_READWRITE |
-              SQLITE_OPEN_CREATE |
-              SQLITE_OPEN_NOMUTEX;
-  int ret = sqlite3_open_v2(_db_name.c_str(), &db, flags, NULL);
+              SQLITE_OPEN_CREATE | SQLITE_OPEN_NOMUTEX;
+  
+  /* initialize database using open_v2(); open() is deprecated */
+  int ret = sqlite3_open_v2(_db_name.c_str(), &db_, flags, NULL);
   if (ret != SQLITE_OK) {
-    string msg = sqlite3_errmsg(db);
+    string msg = sqlite3_errmsg(db_);
     throw StorageException(__FILE__, __LINE__, msg);
   }
 }
 
 SQLiteDB::~SQLiteDB() {
-  sqlite3_close(db);
+  sqlite3_close(db_);
+}
+
+SQLiteDB::Query::Query(sqlite3 *_db, const string& _q) {
+  int ret = sqlite3_prepare_v2(_db, _q.c_str(), _q.length() + 1, &stmt_, NULL);
+  if (ret != SQLITE_OK) {
+    string msg = sqlite3_errmsg(_db);
+    throw StorageException(__FILE__, __LINE__, msg);
+  }
 }
 
 } /* end of namespace S */
--- database/sqlite_db.h
+++ database/sqlite_db.h
@@ -17,13 +17,30 @@ public:
   static Ptr SQLiteDBNew(const string& _db_name) {
     return new SQLiteDB(_db_name);
   }
+  
+  class Query : public Simone::PtrInterface<Query> {
+    friend class SQLiteDB;
+  public:
+    typedef Simone::Ptr<const Query> PtrConst;
+    typedef Simone::Ptr<Query> Ptr;
+    
+  private:
+    Query(sqlite3 *_db, const string& _query);
+    
+    /* data members */
+    sqlite3_stmt *stmt_;
+  };
+
+  Query::PtrConst query(const string & _query) const {
+    return new Query(db_, _query);
+  }
 
 private:
   SQLiteDB(const string& _db_name);
   virtual ~SQLiteDB();
   
   /* data members */
-  sqlite3 *db;
+  sqlite3 *db_;
 };
 
 } /* end of namespace Simone */
