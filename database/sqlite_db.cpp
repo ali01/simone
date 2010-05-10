51f903951aef157331ce208864d55a6d44e45619
Ali Yahya <alive@ali01.com>
2010-05-09 19:03:53 -0700

    preliminary interface for sqlite3 database

--- database/sqlite_db.cpp
+++ database/sqlite_db.cpp
@@ -1,5 +1,13 @@
 #include "sqlite_db.h"
 
+/* lib includes */
+#include <stdint.h>
+
+/* stl includes */
+#include <string>
+using std::string;
+
+/* simone includes */
 #include "../exception.h"
 
 namespace Simone {
@@ -10,7 +18,7 @@ SQLiteDB::SQLiteDB(const string& _db_name) {
                     single sqlite3* abject is always accessed by one thread; */
   int flags = SQLITE_OPEN_READWRITE |
               SQLITE_OPEN_CREATE | SQLITE_OPEN_NOMUTEX;
-  
+
   /* initialize database using open_v2(); open() is deprecated */
   int ret = sqlite3_open_v2(_db_name.c_str(), &db_, flags, NULL);
   if (ret != SQLITE_OK) {
@@ -22,6 +30,144 @@ SQLiteDB::SQLiteDB(const string& _db_name) {
 SQLiteDB::~SQLiteDB() {
   sqlite3_close(db_);
 }
+
+/* SQLiteDB::Row */
+
+SQLiteDB::Row::Row(Query::Ptr _query) : query_(_query), valid_(true) {}
+
+SQLiteDB::Row::ValueType
+SQLiteDB::Row::type(int column_index) const {
+  verify_valid();
+  verify_column_index(column_index);
+
+  ValueType type;
+  int sqlite_type = sqlite3_column_type(query_->stmt_, column_index);
+
+  switch (sqlite_type) {
+    case SQLITE_INTEGER:
+      type = kInteger;
+      break;
+    case SQLITE_FLOAT:
+      type = kFloat;
+      break;
+    case SQLITE_TEXT:
+      type = kText;
+      break;
+    case SQLITE_BLOB:
+      type = kBlob;
+      break;
+    case SQLITE_NULL:
+      type = kNull;
+      break;
+    default:
+      string msg = "sqlite3 value type not recognized";
+      throw InvalidValueException(__FILE__, __LINE__, msg);
+  }
+
+  return type;
+}
+
+double
+SQLiteDB::Row::dbl(int column_index) const {
+  verify_valid();
+  verify_column_index(column_index);
+  verify_numeric(column_index);
+
+  return sqlite3_column_double(query_->stmt_, column_index);
+}
+
+int
+SQLiteDB::Row::integer(int column_index) const {
+  verify_valid();
+  verify_column_index(column_index);
+  verify_numeric(column_index);
+
+  return sqlite3_column_int(query_->stmt_, column_index);
+}
+
+int64_t
+SQLiteDB::Row::integer64(int column_index) const {
+  verify_valid();
+  verify_column_index(column_index);
+  verify_numeric(column_index);
+
+  return sqlite3_column_int64(query_->stmt_, column_index);
+}
+
+string
+SQLiteDB::Row::text(int column_index) const {
+  verify_valid();
+  verify_column_index(column_index);
+
+  ValueType tp = type(column_index);
+  if (tp == kNull || tp == kBlob) {
+    string msg = "sqlite3 value type unexpected";
+    throw UnknownTypeException(__FILE__, __LINE__, msg);
+  }
+  
+  const unsigned char *text = sqlite3_column_text(query_->stmt_, column_index);
+  return string((const char*)text);
+}
+
+int
+SQLiteDB::Row::byteCount(int column_index) const {
+  verify_valid();
+  verify_column_index(column_index);
+
+  ValueType tp = type(column_index);
+  if (tp != kBlob && tp != kText) {
+    string msg = "sqlite3 value type unexpected";
+    throw UnknownTypeException(__FILE__, __LINE__, msg);
+  }
+
+  return sqlite3_column_bytes(query_->stmt_, column_index);
+}
+
+const void *
+SQLiteDB::Row::blob(int column_index) const {
+  verify_valid();
+  verify_column_index(column_index);
+
+  ValueType tp = type(column_index);
+  if (tp != kBlob) {
+    string msg = "sqlite3 value type unexpected";
+    throw UnknownTypeException(__FILE__, __LINE__, msg);
+  }
+
+  if (byteCount(column_index) <= 0) {
+    string  msg = "sqlite zero length blob";
+    throw UnknownTypeException(__FILE__, __LINE__, msg);
+  }
+
+  return sqlite3_column_blob(query_->stmt_, column_index);
+}
+
+void
+SQLiteDB::Row::verify_column_index(int column_index) const {
+  if (column_index >= columnCount()) {
+    string msg = "sqlite3 column index out of range";
+    throw RangeException(__FILE__, __LINE__, msg);
+  }
+}
+
+void
+SQLiteDB::Row::verify_numeric(int column_index) const {
+  ValueType tp = type(column_index);
+  if (tp != kInteger && tp != kFloat && tp != kNull) {
+    string msg = "sqlite3 value type unexpected";
+    throw UnknownTypeException(__FILE__, __LINE__, msg);
+  }
+}
+
+void
+SQLiteDB::Row::verify_valid() const {
+  if (!valid_) {
+    string msg = "operation on invalid sqlite3 table row";
+    throw InvalidOperationException(__FILE__, __LINE__, msg);
+  }
+}
+
+/* SQLiteDB::Query */
 
 SQLiteDB::Query::Query(sqlite3 *_db, const string& _q) {
   int ret = sqlite3_prepare_v2(_db, _q.c_str(), _q.length() + 1, &stmt_, NULL);
--- database/sqlite_db.h
+++ database/sqlite_db.h
@@ -13,23 +13,65 @@ class SQLiteDB : public Simone::PtrInterface<SQLiteDB> {
 public:
   typedef Simone::Ptr<const SQLiteDB> PtrConst;
   typedef Simone::Ptr<SQLiteDB> Ptr;
-  
-  static Ptr SQLiteDBNew(const string& _db_name) {
-    return new SQLiteDB(_db_name);
-  }
-  
+
+  /* forward declaration */
+  class Query;
+
+  /* nested class table row */
+  class Row: public Simone::PtrInterface<Row> {
+    friend class SQLiteDB;
+  public:
+    typedef Simone::Ptr<const Row> PtrConst;
+
+    enum ValueType { kInteger, kFloat, kText, kBlob, kNull };
+
+    ValueType type(int column_index) const;
+
+    double dbl(int column_index) const;
+    int integer(int column_index) const;
+    int64_t integer64(int column_index) const;
+    string text(int column_index) const;
+
+    int byteCount(int column_index) const;
+    const void *blob(int column_index) const;
+
+    int columnCount() const { return sqlite3_column_count(query_->stmt_); }
+    bool valid() const { return valid_; }
+
+  private:
+    Row(Simone::Ptr<Query> _query);
+
+    /* member functions */
+    void verify_column_index(int column_index) const;
+    void verify_numeric(int column_index) const;
+    void verify_valid() const;
+    void validIs(bool _status) { valid_ = _status; }
+
+    /* data members */
+
+    /* smart pointer to guarantee continued existence of
+       sqlite3_stmt* in Query throughout this object's existence */
+    Simone::Ptr<Query> query_;
+    bool valid_;
+  };
+
+  /* nested class query */
   class Query : public Simone::PtrInterface<Query> {
     friend class SQLiteDB;
   public:
     typedef Simone::Ptr<const Query> PtrConst;
     typedef Simone::Ptr<Query> Ptr;
-    
+
   private:
     Query(sqlite3 *_db, const string& _query);
-    
+
     /* data members */
     sqlite3_stmt *stmt_;
   };
+
+  static Ptr SQLiteDBNew(const string& _db_name) {
+    return new SQLiteDB(_db_name);
+  }
 
   Query::PtrConst query(const string & _query) const {
     return new Query(db_, _query);
@@ -38,7 +80,7 @@ public:
 private:
   SQLiteDB(const string& _db_name);
   virtual ~SQLiteDB();
-  
+
   /* data members */
   sqlite3 *db_;
 };
