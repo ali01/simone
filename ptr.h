4dadc95da1f50dbaf19c1bd8167d33e8d1566f86
Ali Yahya <alive@ali01.com>
2010-05-30 15:30:19 -0700

    formatting

--- ptr.h
+++ ptr.h
@@ -1,6 +1,6 @@
 /* Adapted from David R. Cheriton's Advanced Object Oriented Programming from a
-   Modeling & Simulation's Perspective ~ Chapter 5: Memory management with smart
-   pointers. */
+  Modeling & Simulation's Perspective ~ Chapter 5: Memory management with smart
+  pointers. */
 
 #ifndef PTR_H_M10ZP6RI
 #define PTR_H_M10ZP6RI
@@ -10,93 +10,94 @@ namespace Simone {
 template <typename T>
 class Ptr {
 public:
-   Ptr(T* p = 0) : ptr_(p) {
-      if (ptr_) {
-         ptr_->newRef();
-      }
-   }
+  Ptr(T* p = 0) : ptr_(p) {
+    if (ptr_)
+      ptr_->newRef();
+  }
 
-   Ptr(const Ptr<T>& mp) : ptr_(mp.ptr_) {
-      if (ptr_){
-         ptr_->newRef();
-      }
-   }
+  Ptr(const Ptr<T>& mp) : ptr_(mp.ptr_) {
+    if (ptr_)
+      ptr_->newRef();
+  }
 
-   ~Ptr() {
-      if (ptr_) {
-         if (ptr_->deleteRef() == 0) {
-            ptr_ = 0;
-         }
-      }
-   }
+  ~Ptr() {
+    if (ptr_) {
+      if (ptr_->deleteRef() == 0)
+        ptr_ = 0;
+    }
+  }
 
-   Ptr<T>& operator=( const Ptr<T>& mp );
-   Ptr<T>& operator=( Ptr<T>& mp );
-   Ptr<T>& operator=( T* p );
+  Ptr<T>& operator=( const Ptr<T>& mp );
+  Ptr<T>& operator=( Ptr<T>& mp );
+  Ptr<T>& operator=( T* p );
 
-   bool operator==( const Ptr<T>& mp ) const { return ptr_ == mp.ptr_; }
-   bool operator!=( const Ptr<T>& mp ) const { return ptr_ != mp.ptr_; }
-   bool operator==( T* p ) const { return ptr_ == p; }
-   bool operator!=( T* p ) const { return ptr_ != p; }
-   bool operator<(const Ptr<T>& mp) const { return ptr_ < mp.ptr_; }
+  bool operator==( const Ptr<T>& mp ) const { return ptr_ == mp.ptr_; }
+  bool operator!=( const Ptr<T>& mp ) const { return ptr_ != mp.ptr_; }
+  bool operator==( T* p ) const { return ptr_ == p; }
+  bool operator!=( T* p ) const { return ptr_ != p; }
+  bool operator<(const Ptr<T>& mp) const { return ptr_ < mp.ptr_; }
 
-   const T& operator*() const { return *ptr_; }
-   T& operator*() { return *ptr_; }
+  const T& operator*() const { return *ptr_; }
+  T& operator*() { return *ptr_; }
 
-   const T * operator->() const { return ptr_; }
-   T * operator->() { return ptr_; }
-   T * ptr() const { return ptr_; }
+  const T * operator->() const { return ptr_; }
+  T * operator->() { return ptr_; }
+  T * ptr() const { return ptr_; }
 
-   template <class OtherType>
-   operator Ptr<OtherType>() const { return Ptr<OtherType>( ptr_ ); }
+  template <class OtherType>
+  operator Ptr<OtherType>() const { return Ptr<OtherType>(ptr_); }
 
-   struct PointerConversion { int valid; };
-   operator int PointerConversion::*() const {
-      return ptr_ ? &PointerConversion::valid : 0;
-   }
+  struct PointerConversion { int valid; };
+  operator int PointerConversion::*() const {
+    return ptr_ ? &PointerConversion::valid : 0;
+  }
 
-   /* enable use with Simone::ConcurrentCollection */
-   template <typename Collection> 
-   void collectionIs(Collection *_c) const {
-      ptr_->collectionIs(_c);
-   }
+  /* enable use with Simone::ConcurrentCollection */
+  template <typename Collection> 
+  void collectionIs(Collection *_c) const {
+    ptr_->collectionIs(_c);
+  }
+
+  template <typename TargetS, typename SourceS>
+  static Ptr<TargetS> st_cast(Ptr<SourceS> _o) {
+    return static_cast<TargetS*>(_o.ptr());
+  }
+  
+  template <typename TargetD, typename SourceD>
+  static Ptr<TargetD> dyn_cast(Ptr<SourceD> _o) {
+    return dynamic_cast<TargetD*>(_o.ptr());
+  }
+
 protected:
-   T *ptr_;
+  T *ptr_;
 };
 
 template<class T> Ptr<T>&
 Ptr<T>::operator=( const Ptr<T>& mp ) {
-   const T * save = ptr_;
-   ptr_ = mp.ptr_;
-   if( ptr_ ) ptr_->newRef();
-   if( save ) save->deleteRef();
-   return *this;
+  const T * save = ptr_;
+  ptr_ = mp.ptr_;
+  if( ptr_ ) ptr_->newRef();
+  if( save ) save->deleteRef();
+  return *this;
 }
 
 template<class T> Ptr<T>&
 Ptr<T>::operator=( Ptr<T>& mp ) {
-   T * save = ptr_;
-   ptr_ = mp.ptr_;
-   if( ptr_ ) ptr_->newRef();
-   if( save ) save->deleteRef();
-   return *this;
+  T * save = ptr_;
+  ptr_ = mp.ptr_;
+  if( ptr_ ) ptr_->newRef();
+  if( save ) save->deleteRef();
+  return *this;
 }
 
 template<class T> Ptr<T>&
 Ptr<T>::operator=( T* p ) {
-   T * save = ptr_;
-   ptr_ = p;
-   if( ptr_ ) ptr_->newRef();
-   if( save ) save->deleteRef();
-   return *this;
+  T * save = ptr_;
+  ptr_ = p;
+  if( ptr_ ) ptr_->newRef();
+  if( save ) save->deleteRef();
+  return *this;
 }
-
-template <class T, class U>
-Ptr<T> ptr_cast(Ptr<U> mp) {
-   return dynamic_cast<T*>(mp.ptr());
-}
-
-/* TODO: write ptr_const_cast facility */
 
 }
 
--- set.h
+++ set.h
@@ -16,100 +16,91 @@ namespace Simone {
 template <typename T,
           typename Compare=less<T>,
           typename Allocator=allocator<T> >
-class Set : public Simone::PtrInterface<Set<T,Compare,Allocator> >,
-            boost::noncopyable {
+class Set :
+  public Simone::PtrInterface<Set<T,Compare,Allocator> >, boost::noncopyable
+{
 public:
-   // type declarations ==============================================================
-   typedef Simone::Ptr<const Set<T,Compare,Allocator> > PtrConst;
-   typedef Simone::Ptr<Set<T,Compare,Allocator> > Ptr;
-   
-   typedef typename set<T,Compare,Allocator>::iterator iterator;
-   typedef typename set<T,Compare,Allocator>::reverse_iterator reverse_iterator;
-   typedef typename set<T,Compare,Allocator>::const_iterator const_iterator;
-   typedef typename set<T,Compare,Allocator>::const_reverse_iterator
-                                                               const_reverse_iterator;
-   
-   // factory constructor ============================================================
-   static Ptr SetNew() { return new Set(); }
-   Set() {}
-   
-   virtual ~Set() {}
-   
-   // iterators ======================================================================
-   iterator begin() {
-      return set_.begin();  
-   }
-   
-   iterator end() {
-      return set_.end();    
-   }
-   
-   reverse_iterator rbegin() {
-      return set_.rbegin(); 
-   }
-   
-   reverse_iterator rend() {
-      return set_.rend();   
-   }
-   
-   
-   const_iterator begin() const {
-      return set_.begin();  
-   }
+  typedef Simone::Ptr<const Set<T,Compare,Allocator> > PtrConst;
+  typedef Simone::Ptr<Set<T,Compare,Allocator> > Ptr;
 
-   const_iterator end() const {
-      return set_.end();    
-   }
+  typedef typename set<T,Compare,Allocator>::iterator iterator;
+  typedef typename set<T,Compare,Allocator>::reverse_iterator reverse_iterator;
+  typedef typename set<T,Compare,Allocator>::const_iterator const_iterator;
+  typedef typename set<T,Compare,Allocator>::const_reverse_iterator
+                                             const_reverse_iterator;
 
-   const_reverse_iterator rbegin() const {
-      return set_.rbegin(); 
-   }
+  static Ptr SetNew() { return new Set(); }
+  Set() {}
 
-   const_reverse_iterator rend() const {
-      return set_.rend();   
-   }
+  virtual ~Set() {}
 
-   
-   // accessors  =====================================================================
-   size_t size() const {
-      return set_.size();  
-   }
+  iterator begin() {
+    return set_.begin();
+  }
 
-   bool empty() const {
-      return set_.empty(); 
-   }
+  iterator end() {
+    return set_.end();
+  }
 
-   
-   iterator element(const T& _v) {
-      return set_.find(_v); 
-   }
+  reverse_iterator rbegin() {
+    return set_.rbegin();
+  }
 
-   const_iterator element(const T& _v) const {
-      return set_.find(_v); 
-   }
+  reverse_iterator rend() {
+    return set_.rend();
+  }
 
-   
-   size_t count(const T& _v) const {
-      return set_.count(_v); 
-   }
-   
-   void clear() {
-      set_.clear();
-   }
+  const_iterator begin() const {
+    return set_.begin();
+  }
 
-   
-   // mutators =======================================================================
-   void elementIs(const T& _v) {
-      set_.insert(_v); 
-   }
+  const_iterator end() const {
+    return set_.end();
+  }
 
-   void elementDel(const T& _v) {
-      set_.erase(_v);
-   }
+  const_reverse_iterator rbegin() const {
+    return set_.rbegin();
+  }
+
+  const_reverse_iterator rend() const {
+    return set_.rend();
+  }
+
+  size_t size() const {
+    return set_.size();
+  }
+
+  bool empty() const {
+    return set_.empty();
+  }
+
+  iterator element(const T& _v) {
+    return set_.find(_v);
+  }
+
+  const_iterator element(const T& _v) const {
+    return set_.find(_v);
+  }
+
+  size_t count(const T& _v) const {
+    return set_.count(_v);
+  }
+
+  void clear() {
+    set_.clear();
+  }
+
+  void elementIs(const T& _v) {
+    set_.insert(_v);
+  }
+
+  void elementDel(const T& _v) {
+    set_.erase(_v);
+  }
+
 private:
-   // member functions ===============================================================
-   // data members ===================================================================
-   set<T,Compare,Allocator> set_;
+  /* data members */
+  set<T,Compare,Allocator> set_;
 };
 
 } /* end of namespace Simone */
