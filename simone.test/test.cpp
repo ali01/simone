76f44c859514263fa28477c902edf2837f812819
Ali Yahya <alive@ali01.com>
2010-07-18 12:58:46 -0700

    removed boost dependencies in test

--- simone.test/test.cpp
+++
@@ -1,8 +0,0 @@
-/* Copyright (c) 2008-2010. Ali H. Yahya, All rights reserved. */
-
-#define BOOST_TEST_MAIN
-#include <boost/test/included/unit_test.hpp>
-
-#include "../time/time.test/time_test.h"
-#include "../network/network.test/network_test.h"
-#include "../thread/thread.test/thread_test.h"
